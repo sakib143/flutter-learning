93dc7c55151ef7c0db595e63da3dd017dab0a61a
DESKTOP-9RET899\syeds <syed.sakib143@gmail.com>
2021-08-02 17:09:53 +0530

    Validation and ripple effect on container.......Sakib Syed

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -11,6 +11,22 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if(_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(
+          context, MyRoutess.homeRoute);
+      //Restore if user back
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,72 +39,78 @@ class _LoginPageState extends State<LoginPage> {
         ),
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  'images/login.png',
-                  fit: BoxFit.cover,
-                ),
-                Text("Welcome $name", style: TextStyle(fontSize: 20.0)),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 32.0),
-                  child: Column(children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter user name", labelText: "User name"),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {}); // refresh UI
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter user password",
-                        labelText: "Password",
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'images/login.png',
+                    fit: BoxFit.cover,
+                  ),
+                  Text("Welcome $name", style: TextStyle(fontSize: 20.0)),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 32.0),
+                    child: Column(children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "Enter user name", labelText: "User name"),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {}); // refresh UI
+                        },
+                        validator: (value) {
+                          if(value!.isEmpty) {
+                            return 'User name can not be empty';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter user password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if(value!.isEmpty) {
+                            return 'Please enter password';
+                          } else if (value.length < 6) {
+                            return 'Password lengh must be greater than 6';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                    ]),
+                  ),
+                  Material(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
+                    child: InkWell(
+                      onTap: () => moveToHome(context),
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        alignment: Alignment.center,
+                        width: changeButton ? 50 : 150,
+                        height: 50.0,
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              ),
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                  ]),
-                ),
-                Material(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
-                  child: InkWell(
-                    onTap: () async {
-                      setState(() {
-                        changeButton = true;
-                      }); // For update UI
-                      await Future.delayed(Duration(seconds: 1));
-                      await Navigator.pushNamed(
-                          context, MyRoutess.homeRoute); //Restore if user back
-                      //Restore if user back
-                      setState(() {
-                        changeButton = false;
-                      });
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      alignment: Alignment.center,
-                      width: changeButton ? 50 : 150,
-                      height: 50.0,
-                      child: changeButton
-                          ? Icon(
-                              Icons.done,
-                              color: Colors.white,
-                            )
-                          : Text(
-                              'Login',
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
-                            ),
-                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
